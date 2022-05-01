54ecd035277c86a0ef40023b3648727167a2ccbd
oracle2_dev <assia-jean.ngoran@wealthtechinnovations.com>
2022-05-01 13:24:49 +0000

    CRUD categories

--- app/Controllers/Http/CategoriesController.ts
+++ app/Controllers/Http/CategoriesController.ts
@@ -2,8 +2,87 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
 import Categorie from 'App/Models/Categorie'
+import Application from '@ioc:Adonis/Core/Application'
 
 export default class CategoriesController {
+  
+    public async page({view}: HttpContextContract){
+      const categories = await Database.from('categories').select('*').where({status:0})
+      console.log(categories)
+      return view.render('categories/list',{categories})
+    }
+  
+    public async create({view}: HttpContextContract){
+      return view.render('categorie/add')
+    }
+  
+    public async ajouter({request, response}: HttpContextContract){
+      const name = request.input('name')
+      const image = request.file('image',{
+        size:'2mb',
+        extnames:['jpg','JPG','png', 'PNG', 'jpeg', 'JPEG']
+      })
+      
+      if(!image){
+        return
+      }
+      
+      if(!image.isValid){
+        return image.errors
+      }
+      
+      await image.move(Application.publicPath('images'))
+      
+      const categorie = await Categorie.create({
+        name: name,
+        image: image.fileName
+      })
+      
+      console.log(image.filePath)
+      response.redirect('/listesCategories')
+    }
+  
+    public async affichermodifier({params,view}: HttpContextContract){
+    const id = params.id
+    const categorie = await Database.from('categories').select('*').where({id:id, status:0})
+      const name = categorie[0].name
+      const image = categorie[0].image
+      const identifiant = categorie[0].id
+      console.log(categorie);
+      return view.render('categories/edit',{name, image, identifiant})
+    }
+
+    public async modifier({params,response, request}: HttpContextContract){
+      const id = params.id
+      const name = request.input('name')
+      const image = request.file('image',{
+        size:'2mb',
+        extnames:['jpg','JPG','png', 'PNG', 'jpeg', 'JPEG']
+      })
+
+      const categorie = await Categorie.findOrFail(id)
+      
+      if(!image){
+        //si ya pas d'image dans le champs, on modifier juste la categorie
+        categorie.name = name
+        await categorie.save()
+        return response.redirect('/affichermodification/'+categorie.id)
+      }else{
+        //si ya une image dans le champ
+        if(!image.isValid){
+          return image.errors
+        }
+        await image.move(Application.publicPath('images'))
+        let nomImage:any;
+        nomImage = image.fileName
+        categorie.name = name
+        categorie.image = nomImage
+        return response.redirect('/affichermodification/'+categorie.id)
+      }
+      
+    }
+    
+    //API
     public async index({response}: HttpContextContract) {
         //recuperation de la liste de toutes les catégories
         try {
@@ -23,9 +102,9 @@ export default class CategoriesController {
     }
 
     public async store({response, request}: HttpContextContract) {
-        //ajouter une nouvelle catégorie
-        try {
-          const name = request.input('name')
+      //ajouter une nouvelle catégorie
+      try {
+        const name = request.input('name')
           const image = request.input('image')
     
           if (!(name || image)) {
@@ -54,6 +133,16 @@ export default class CategoriesController {
             message: 'Une erreur est survenue!',
           })
         }
+    }
+
+    public async voir({view, params}: HttpContextContract){
+      const {id} = params
+      const categories = await Database.from('categories').select('*').where({id:id, status:0})
+      const ide = categories[0].id
+      const name = categories[0].name
+      const image = categories[0].image
+      const date = categories[0].created_at
+      return view.render('categories/view',{name,image,date,ide})
     }
 
     public async show({response,params}: HttpContextContract) {
