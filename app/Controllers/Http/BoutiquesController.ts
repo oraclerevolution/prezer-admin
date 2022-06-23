26a7dc55ba67fe43589d100cf90763abbcf5d613
oracle2_dev <mac.n.drix@gmail.com>
2022-06-23 00:33:51 +0000

    Affichage des boutiques et de leurs détails

--- app/Controllers/Http/BoutiquesController.ts
+++ app/Controllers/Http/BoutiquesController.ts
@@ -1,3 +1,26 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
 
-export default class BoutiquesController {}
+export default class BoutiquesController {
+    public async index({response}: HttpContextContract){
+        const boutiques = await Database.from('boutiques').select('*').where({status:0})
+        return response.json({
+            error: false,
+            message: 'La liste des boutiques obtenue avec succès!',
+            status:200,
+            boutiques
+          })
+    }
+
+    public async show({response, params}: HttpContextContract){
+        const id = params.id_boutique
+        const detailsBoutique = await Database.from('boutiques').select('*').where({id:id})
+        return response.json({
+            error: false,
+            message: "Détails d'une boutique obtenus avec succès",
+            status: 200,
+            detailsBoutique
+        })
+    }
+}
