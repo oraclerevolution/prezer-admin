dd307fd41185398d4d37da1e15f6674468d8a554
oracle2_dev <mac.n.drix@gmail.com>
2022-06-23 00:37:27 +0000

    Modification du controller des Categories

--- app/Controllers/Http/CategoriesController.ts
+++ app/Controllers/Http/CategoriesController.ts
@@ -2,87 +2,9 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
 import Categorie from 'App/Models/Categorie'
-import Application from '@ioc:Adonis/Core/Application'
-import Moment from 'moment'
 
 export default class CategoriesController {
   
-    public async page({view}: HttpContextContract){
-      const categories = await Database.from('categories').select('*').where({status:0})
-      const publicPath = Application.configPath('images')
-      return view.render('categories/list',{categories, publicPath})
-    }
-  
-    public async create({view}: HttpContextContract){
-      return view.render('categorie/add')
-    }
-  
-    public async ajouter({request, response}: HttpContextContract){
-      const name = request.input('name')
-      const image = request.file('image',{
-        size:'2mb',
-        extnames:['jpg','JPG','png', 'PNG', 'jpeg', 'JPEG']
-      })
-      
-      if(!image){
-        return
-      }
-      
-      if(!image.isValid){
-        return image.errors
-      }
-      
-      await image.move(Application.publicPath('images'))
-      
-      const categorie = new Categorie
-
-      categorie.name = name
-      categorie.image = `images/${image.fileName}`
-      await categorie.save()
-
-      response.redirect('/listesCategories')
-    }
-  
-    public async affichermodifier({params,view}: HttpContextContract){
-      const id = params.id
-      const categorie = await Database.from('categories').select('*').where({id:id, status:0})
-      const name = categorie[0].name
-      const image = categorie[0].image
-      const identifiant = categorie[0].id
-      console.log(categorie);
-      return view.render('categories/edit',{name, image, identifiant})
-    }
-
-    public async modifier({params,response, request}: HttpContextContract){
-      const id = params.id
-      const name = request.input('name')
-      const image = request.file('image',{
-        size:'2mb',
-        extnames:['jpg','JPG','png', 'PNG', 'jpeg', 'JPEG']
-      })
-
-      const categorie = await Categorie.findOrFail(id)
-      
-      if(!image){
-        //si ya pas d'image dans le champs, on modifier juste la categorie
-        categorie.name = name
-        await categorie.save()
-        return response.redirect('/affichermodification/'+categorie.id)
-      }else{
-        //si ya une image dans le champ
-        if(!image.isValid){
-          return image.errors
-        }
-        await image.move(Application.publicPath('images'))
-        let nomImage:any;
-        nomImage = image.fileName
-        categorie.name = name
-        categorie.image = nomImage
-        return response.redirect('/affichermodification/'+categorie.id)
-      }
-      
-    }
-    
     //API
     public async index({response}: HttpContextContract) {
         //recuperation de la liste de toutes les catégories
@@ -134,15 +56,6 @@ export default class CategoriesController {
             message: 'Une erreur est survenue!',
           })
         }
-    }
-
-    public async voir({view, params}: HttpContextContract){
-      const {id} = params
-      const categories = await Database.from('categories').select('*').where({id:id, status:0})
-      const dateParse = Moment(categories[0].created_at).format('d MMM YYYY')
-      console.log(categories)
-      
-      return view.render('categories/view',{categories:categories, dateParse})
     }
 
     public async show({response,params}: HttpContextContract) {
